40ad03d709afe14a996e17e16b3d7bdb3260d94c
Anaecy28 <Anaecygaray@gmail.com>
2025-01-31 21:34:43 +0000

    release version 1.0

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit as st 
 import pandas as pd 
-from google.cloud 
-import firestore 
-from google.oauth2 
-import service_account
+from google.cloud import firestore 
+from google.oauth2 import service_account
 import json 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict) 
