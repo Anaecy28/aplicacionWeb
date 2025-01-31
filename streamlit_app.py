ea6df4d54f35621e2a9ac1beb1f199f485cf1fe4
Anaecy28 <Anaecygaray@gmail.com>
2025-01-31 23:50:45 +0000

    release version 1.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import json
 #key_dict = json.loads(st.secrets["textkey"])
 #creds = service_account.Credentials.from_service_account_info(key_dict) 
 #db = firestore.Client(credentials=creds, project="curso-fea49") 
-db = firestore.Client.from_service_account_json("names-firebase.json")
+db = firestore.Client.from_service_account_json("firebase.json")
 
 dbNames = db.collection("names") 
 st.header("Nuevo registro") 
